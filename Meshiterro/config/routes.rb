b92c19c5ca3295d4b7c0014425cbf61c6b8696fd
sykg1204yuki <y.yuki040507@gmail.com>
2021-06-15 02:54:53 +0000

    バリデーションまで完成

--- Meshiterro/config/routes.rb
+++ Meshiterro/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   resources :post_images, only:[:new, :create, :index, :show, :destroy] do
@@ -7,5 +8,7 @@ Rails.application.routes.draw do
     
     resources :post_comments, only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
+  
 
 end
